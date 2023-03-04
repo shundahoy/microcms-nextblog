3eb834f1cf85f6a12ca3814ba3c915142d798de3
shundahoy <shundahoy@gmail.com>
2023-03-05 01:07:44 +0900

    first commit

---
+++ libs/client.js
@@ -0,0 +1,7 @@
+// libs/client.js
+import { createClient } from "microcms-js-sdk";
+
+export const client = createClient({
+  serviceDomain: process.env.DOMAIN,
+  apiKey: process.env.API_KEY,
+});
---
+++ pages/blog/[id].js
@@ -0,0 +1,44 @@
+import { client } from "../../libs/client";
+import styles from "../../styles/Home.module.scss";
+import cheerio from "cheerio";
+import hljs from "highlight.js";
+import "highlight.js/styles/night-owl.css";
+
+export default function BlogId({ blog, highlightedBody }) {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>{blog.title}</h1>
+      <p className={styles.publishedAt}>{blog.publishedAt}</p>
+      <div dangerouslySetInnerHTML={{ __html: highlightedBody }}></div>
+    </main>
+  );
+}
+
+// 静的生成のためのパスを指定します
+export const getStaticPaths = async () => {
+  const data = await client.get({ endpoint: "blog" });
+
+  const paths = data.contents.map((content) => `/blog/${content.id}`);
+  return { paths, fallback: false };
+};
+
+// データをテンプレートに受け渡す部分の処理を記述します
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const data = await client.get({ endpoint: "blog", contentId: id });
+
+  const $ = cheerio.load(data.body);
+
+  $("pre code").each((_, elm) => {
+    const result = hljs.highlightAuto($(elm).text());
+    $(elm).html(result.value);
+    $(elm).addClass("hljs");
+  });
+
+  return {
+    props: {
+      blog: data,
+      highlightedBody: $.html(),
+    },
+  };
+};
